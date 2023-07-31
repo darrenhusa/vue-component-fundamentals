572c7e06219072dc4ca532c2e416474a475d0e27
Darren Henderson <darrenh@ccsj.edu>
2023-07-31 00:18:35 -0500

    Finished video 7.

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,5 +1,5 @@
-let PlanComponent = {
-    template: '#plan-template',
+let PlanPickerItemComponent = {
+    template: '#plan-picker-item-template',
     props: {
         name: {type: String, required: true},
         selected: {type: Boolean, default: false}
@@ -14,7 +14,7 @@ let PlanComponent = {
 
 let PlanPickerComponent = {
     
-    components: {plan: PlanComponent},
+    components: {'plan-picker-item': PlanPickerItemComponent},
 
     template: '#plan-picker-template',
     
